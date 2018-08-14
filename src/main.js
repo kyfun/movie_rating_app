a352d677fa0369eeb2ca1dea68f78011b4f9e221
KYF <fun921@gmail.com>
2018-08-15 00:43:24 +0800

    Commit: Create movie list

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import BootstrapVue from 'bootstrap-vue';
-
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App';
@@ -11,13 +10,11 @@ import router from './router';
 
 Vue.use(BootstrapVue);
 Vue.use(Vuetify);
-
 Vue.config.productionTip = false;
-
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
+el: '#app',
+router,
+components: { App },
+template: '<App/>',
 });
